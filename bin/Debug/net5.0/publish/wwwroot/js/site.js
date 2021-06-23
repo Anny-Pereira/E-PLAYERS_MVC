ad67e9d76d36248b842aec9cdfe03cc837f8ff9d
Anny Pereira <isabelle.pereira04@gmail.com>
2021-06-23 19:13:04 -0300

    Publish Feito

---
+++ bin/Debug/net5.0/publish/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function BotaoMenu() {
+    var menu = document.getElementById("itens_header")
+    if (menu.style.display === "flex") {
+        menu.style.display = "none";
+     //quando o menu estiver em display:flex, o esconda
+    }
+    else {
+        menu.style.display = "flex";
+     //se o menu não estiviver em display:flex, o mostre
+    }
+}
